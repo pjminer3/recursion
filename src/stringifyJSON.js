12db2a73dc7b7ec262dd8dddea57fe7cb6113aba
Patrick Miner <patrickminer318@gmail.com>
2017-09-06 15:00:50 -0700

    Passed stringifyJSON specs

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,56 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  let result = '';
+
+  function stringify(o) {
+    if (o === undefined) {
+      return;
+    } else if (typeof o === 'function') {
+      return;
+    }
+
+    if (typeof o === 'number') {
+      result += o.toString();
+    } else if (typeof o === 'string') {
+      result += ('"' + o + '"');
+    } else if (typeof o === 'boolean') {
+      result += o.toString();
+    } else if (o === null) {
+      result += 'null';
+    } else {
+      // The cases below are if the obj is truly an object
+      if (Array.isArray(o) === true) {
+        // if the object is an array
+        result += '[';
+        for (let i = 0; i < o.length - 1; i++) {
+          stringify(o[i]);
+          result += ',';
+        }
+        stringify(o[o.length - 1]);
+        result += ']';
+      } else {
+        // if the object is an object
+        result += '{';
+        if (Object.keys(o).length > 0) {
+          for (let i in o) {
+            if (o[i] === undefined || typeof o[i] === 'function') {
+            } else {
+              stringify(i);
+              result += ':';
+              stringify(o[i]);
+              result += ',';
+            }
+          }
+          if (result.length > 1) {
+            result = result.slice(0, result.length -1);
+          }
+        }
+        result += '}';
+      }
+    }
+  }
+  
+  stringify(obj);
+  return result;
 };
